e57bbb5ad775d28d8103f1abd7ad2883ddf9d917
Krzysztof Sakowski <krz.sakowski@gmail.com>
2016-05-21 19:09:34 +0200

    Added input parsing.

--- ks335487.pl
+++ ks335487.pl
@@ -1,1 +1,29 @@
 % Krzysztof Sakowski
+:- ensure_loaded([library(lists), library(file_systems)]). % TODO file_systems lib?
+
+verify(ProcAmt, FileName) :-
+    validateProcAmt(ProcAmt),
+    readProgram(FileName, Data),
+    write(Data), nl.
+
+validateProcAmt(ProcAmt) :-
+    ( ProcAmt =< 0 ->
+        write('Error: parametr 0 powinien byc liczba > 0'), nl, !, fail
+    ;
+        true
+    ).
+
+readProgram(FileName, Data) :-
+    ( file_exists(FileName) ->
+        open(FileName, read, Str),
+        parseProgram(Str, Data),
+        close(Str)
+    ;
+        write('Error: brak pliku o nazwie - '), write(FileName), nl, !, fail
+    ).
+
+parseProgram(Str, Data) :-
+    read(Str, Vars),
+    read(Str, Arrs),
+    read(Str, Ins),
+    Data = [Vars, Arrs, Ins].
