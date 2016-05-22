881ceac81abb1d0b46acd693337ff73e286fe013
Krzysztof Sakowski <krz.sakowski@gmail.com>
2016-05-22 18:38:38 +0200

    Added generating init state.

--- ks335487.pl
+++ ks335487.pl
@@ -1,13 +1,14 @@
 % Krzysztof Sakowski
 :- ensure_loaded([library(lists), library(file_systems)]). % TODO file_systems lib?
 
-verify(ProcAmt, FileName) :-
-    validateProcAmt(ProcAmt),
+verify(N, FileName) :-
+    validateProcAmt(N),
     readProgram(FileName, Data),
-    write(Data), nl.
+    write(Data), nl,
+    initState(Data, N, Out).
 
-validateProcAmt(ProcAmt) :-
-    ( ProcAmt =< 0 ->
+validateProcAmt(N) :-
+    ( N =< 0 ->
         write('Error: parametr 0 powinien byc liczba > 0'), nl, !, fail
     ;
         true
@@ -27,3 +28,55 @@ parseProgram(Str, Data) :-
     read(Str, Arrs),
     read(Str, Ins),
     Data = [Vars, Arrs, Ins].
+
+
+% [(zmienna, wartość)]
+% [(tablica, [lista_wartosci])]
+% [kolejcna instrukcja dla proc]
+
+% getVarList(Program, Vars) :-
+
+
+getInstrList(Program, Ins) :-
+    nth1(3, Program, program(Ins)).
+
+% initState(+Program, +N, -StanPoczątkowy)
+initState(Program, N, InitState) :-
+    initVariablesWithZeros(Program, InitVars),
+    initArrsWithZeros(Program, N, InitArrs),
+    generateListWithZeros(N, 1, InitInstr),
+    write([InitVars, InitArrs, InitInstr]), nl,
+    InitState = [InitVars, InitArrs, InitInstr].
+
+initVariablesWithZeros(Program, InitVars) :-
+    nth1(1, Program, vars(Vars)),
+    initVariablesWithZeros2(Vars, InitVars).
+
+initVariablesWithZeros2([], []).
+initVariablesWithZeros2([X|Xs], [Y|Ys]) :-
+    Y = (X, 0),
+    initVariablesWithZeros2(Xs, Ys).
+
+initArrsWithZeros(Program, N, InitArrs) :-
+    nth1(2, Program, arrays(Arrs)),
+    initArrsWithZeros2(Arrs, N, InitArrs).
+
+initArrsWithZeros2([], _, []).
+initArrsWithZeros2([X|Xs], N, [Y|Ys]) :-
+    generateListWith(N, 0, List),
+    Y = (X, List),
+    initArrsWithZeros2(Xs, N, Ys).
+
+% generateListWith(+Size, +Value, -List)
+generateListWith(0, _, []).
+generateListWith(N, Val, [X|Xs]) :-
+    X is Val,
+    M is N-1,
+    generateListWith(M, Val, Xs).
+
+
+
+
+
+
+% step(+Program, +StanWe, ?PrId, -StanWy)
